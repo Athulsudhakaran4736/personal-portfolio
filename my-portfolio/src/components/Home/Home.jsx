f6d0b5af8c78b58e660dd022b2d5c175bd2b1a09
Athul Sudhakaran <57166885+Athulsudhakaran4736@users.noreply.github.com>
2024-05-18 12:51:19 +0530

    test2

--- my-portfolio/src/components/Home/Home.jsx
+++ my-portfolio/src/components/Home/Home.jsx
@@ -4,93 +4,87 @@ import HomeExperience from "../HomeExperience/HomeExperience";
 import HomeSkills from "../HomeSkills/HomeSkills";
 import HomeStory from "../HomeStory/HomeStory";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
-import { useMemo } from "react";
-import HomeProject from "../HomeProject/HomeProject";
+// import { useEffect } from "react";
+// import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadSlim } from "@tsparticles/slim";
+// import { useMemo } from "react";
+// import HomeProject from "../HomeProject/HomeProject";
 
 const Home = () => {
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
-      await loadSlim(engine);
-      //await loadBasic(engine);
-    });
-  }, []);
-  const options = useMemo(
-    () => ({
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 1,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: false,
-          speed: 4,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-            // area: 800,
-          },
-          value: 30,
-        },
-        opacity: {
-          value: 0.4,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      detectRetina: true,
-    }),
-    [],
-  );
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  // useEffect(() => {
+  //   initParticlesEngine(async (engine) => {
+  //     await loadSlim(engine);
+  //   });
+  // }, []);
+  // const options = useMemo(
+  //   () => ({
+  //     fpsLimit: 120,
+  //     interactivity: {
+  //       events: {
+  //         onClick: {
+  //           enable: true,
+  //           mode: "push",
+  //         },
+  //         onHover: {
+  //           enable: true,
+  //           mode: "repulse",
+  //         },
+  //       },
+  //       modes: {
+  //         push: {
+  //           quantity: 1,
+  //         },
+  //         repulse: {
+  //           distance: 200,
+  //           duration: 0.4,
+  //         },
+  //       },
+  //     },
+  //     particles: {
+  //       color: {
+  //         value: "#ffffff",
+  //       },
+  //       links: {
+  //         color: "#ffffff",
+  //         distance: 150,
+  //         enable: true,
+  //         opacity: 0.4,
+  //         width: 1,
+  //       },
+  //       move: {
+  //         direction: "none",
+  //         enable: true,
+  //         outModes: {
+  //           default: "bounce",
+  //         },
+  //         random: false,
+  //         speed: 4,
+  //         straight: false,
+  //       },
+  //       number: {
+  //         density: {
+  //           enable: true,
+  //           // area: 800,
+  //         },
+  //         value: 30,
+  //       },
+  //       opacity: {
+  //         value: 0.4,
+  //       },
+  //       shape: {
+  //         type: "circle",
+  //       },
+  //       size: {
+  //         value: { min: 1, max: 5 },
+  //       },
+  //     },
+  //     detectRetina: true,
+  //   }),
+  //   [],
+  // );
+  // const particlesLoaded = (container) => {
+  //   console.log(container);
+  // };
   return (
     <>
       <div className="home-container">
